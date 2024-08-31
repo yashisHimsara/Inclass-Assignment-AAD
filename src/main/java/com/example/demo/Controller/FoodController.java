c382c698547fa02f31df0da832fd0640d2baa84e
yashisHimsara <yasissenavirathna@gmail.com>
2024-08-31 10:52:25 +0530

    create

---
+++ src/main/java/com/example/demo/Controller/FoodController.java
@@ -0,0 +1,12 @@
+package com.example.demo.Controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+
+@WebServlet(urlPatterns = "/Food")
+public class FoodController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, Hgi)
+}
